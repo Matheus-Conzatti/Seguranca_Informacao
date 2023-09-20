5645087c312321dbf99c0463c74dfa4980c0ca27
Matheus Conzatti de Souza <matheus_conzatti@outlook.com>
2023-09-20 12:01:04 -0300

    Arquivo diconario Feito

--- dicionario.php
+++ dicionario.php
@@ -1,7 +1,7 @@
 <?php
-    //Verifica se o formulario foi envido
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        //Obtem o hash alvo do formulario
+    //Verifica se o formulário foi enviado
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        //Obtém o hash alvo do formulário
         $hashAlvo = $_POST["hashAlvo"];
 
         //Configurações do banco de dados
@@ -13,9 +13,9 @@
         //Conecta ao banco de dados
         $conn = new mysqli($host, $usuarioBD, $senhaBD, $bancoDados);
 
-        //Verifica se a conexão foi bem-sucedida
+        // Verifica se a conexão foi bem-sucedida
         if($conn->connect_error){
-            die("Erro na conexão com o banco de dados:". $conn->connect_error);
+            die("Erro na conexão com o banco de dados: " . $conn->connect_error);
         }
 
         //Consulta para verificar a senha
@@ -25,23 +25,41 @@
         $stmt->execute();
         $stmt->store_result();
 
-        if($smtm->num_row > 0){
-            //Senha encontrada do banco de dados
-            $smtm->bind_result($senhaEncontra);
-            $smtm->fetch();
+        if($stmt->num_rows > 0){
+            //Senha encontrada no banco de dados
+            $stmt->bind_result($senhaEncontrada);
+            $stmt->fetch();
 
             //Salve a senha em um arquivo
             $arquivoSalvo = "Senha_encontrada.txt";
-            file_put_contents($arquivoSalvo, $senhaEncontra);
-            echo "Senha encontrada no banco de dados: $senhaEncontrada e salva em $arquivoSalvo";
+            file_put_contents($arquivoSalvo, $senhaEncontrada);
+            echo "Senha encontrada no banco de dados: $senhaEncontrada e salva em $arquivoSalvo.";
+            echo "<br>";
+            echo "<input type='button' value='Voltar' onclick='history.go(-1)'/>";
+            echo "<script>
+                $(document).ready(function(){
+                $(window).scroll(function(){
+                    if ($(this).scrollTop() > 100) {
+                    $('a[href='#top']').fadeIn();
+                    } else {
+                    $('a[href='#top']').fadeOut();
+                    }
+                });
+        
+                $('a[href='#top']').click(function(){
+                    $('html, body').animate({scrollTop : 0},800);
+                    return false;
+                });
+                });
+            </script>";
         }else{
             echo "<script language='javascript'>
                 alert('Senha não encontrada no banco de dados!');
-                location.href='dicionario.html'
+                location.href='dicionario.html';
             </script>";
         }
 
-        //Fecha a conexão e a consulta 
+        // Fecha a conexão e a consulta
         $stmt->close();
         $conn->close();
     }
